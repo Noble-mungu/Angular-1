104ded10adcbca5284c6b68b1b746d0626c37271
Noble Mungu <mungunoble@gmail.com>
2020-06-21 21:02:00 +0300

    add form component and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { DateCountPipe } from './date-count.pipe';
 import { HighlightDirective } from './highlight.directive';
-
-import { AppComponent } from './app.component';
+import { DateCountPipe } from './date-count.pipe';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
+    FormComponent,
     HighlightDirective,
-
+    DateCountPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
